f82ee5f46da3eadd917aed628cafd074ef29c323
erikvw <ew2789@gmail.com>
2019-04-09 17:09:26 -0500

    black

--- edc_reports/tests/test_report.py
+++ edc_reports/tests/test_report.py
@@ -5,10 +5,8 @@ from .my_report import MyReport
 
 
 class TestReport(TestCase):
-
     def test_report(self):
         report = MyReport(filename=f"{uuid4()}.pdf")
         response = report.render()
         self.assertEqual(response.status_code, 200)
-        self.assertIn("ReportLab Generated PDF document",
-                      str(response.content))
+        self.assertIn("ReportLab Generated PDF document", str(response.content))
