ea37460fb3f4814e44e844b0f86a145934b6745c
erikvw <ew2789@gmail.com>
2023-08-01 00:13:10 -0500

    update tests  [skip ci]

--- runtests.py
+++ runtests.py
@@ -17,6 +17,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
     calling_file=__file__,
     BASE_DIR=base_dir,
     APP_NAME=app_name,
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
     EDC_AUTH_SKIP_SITE_AUTHS=True,
     EDC_AUTH_SKIP_AUTH_UPDATER=True,
     EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
